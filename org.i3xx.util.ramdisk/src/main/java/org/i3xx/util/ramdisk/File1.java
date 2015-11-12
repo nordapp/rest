eac99fe8eb178792a0521cc95d1bf81d460d2f30
Stefan <Stefan.Hauptmann@ids-dialogsysteme.net>
2015-11-12 13:41:26 +0100

    Fix import

---
+++ org.i3xx.util.ramdisk/src/main/java/org/i3xx/util/ramdisk/File1.java
@@ -0,0 +1,9 @@
+package org.i3xx.util.ramdisk;
+
+public class File1 {
+
+	public File1() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ org.i3xx.util.ramdisk/src/test/java/org/i3xx/util/ramdisk/File1Test.java
@@ -0,0 +1,24 @@
+package org.i3xx.util.ramdisk;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class File1Test {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
